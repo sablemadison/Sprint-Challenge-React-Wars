3b234be4860fc5337cae6fb0ec6f987944d6714f
Sable <sablechilds@yahoo.com>
2019-08-05 20:26:40 -0400

    added-star-wars-card-component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import Cards from './Cards';
 
 
 const App = () => {
@@ -10,12 +11,12 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [character, setCharacter] = useState([]);
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
 axios
 .get("http https://swapi.co/api/people/")
-.then( res => {console.log('data', res.data.res)
-setCharacter(res.data.res)}
+.then( res => {console.log('data:', res.data.res)
+setCharacters(res.data.res)}
 )
 .catch(err => {console.log('Error:', err)}
 )
